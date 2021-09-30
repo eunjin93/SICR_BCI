30978729ec670635928f972d5286a8857e3cac64
Eunjin Jeon <37999008+eunjin93@users.noreply.github.com>
2021-09-30 23:33:43 +0900

    Update Setting.py

--- Setting.py
+++ Setting.py
@@ -1,32 +1,23 @@
+import os
 import Utils as ut
 
 
 """ Path """
-path = "/DataCommon/ejjeon/"
-project_path = path + "Projects_BCI/SCIR/"
+project_path = "./"
+model_path = ut.create_dir(project_path + "Model/")
+result_path = ut.create_dir(project_path+"Result/")
+tensorboard_path = ut.create_dir(project_path+"Tensorboard/")
 
+raw_data_path = "/home/ejjeon/DATA/data/[GIST] Raw_MI_data/"
+preprocessed_data_path = "/home/ejjeon/DATA/data/[GIST] preprocessed data/npy/"
 
-KU_time_path = path + "data/KU_Motor_Imagery/TIME/5-fold/"
-GIST_time_path = path + "data/GIST_Motor_Imagery/TIME/5-fold/"
-
-model_path = ut.create_dir(project_path + "Save_model/")
-result_path = ut.create_dir(project_path + "Results/")
-selected_path = ut.create_dir(project_path + "Selected/")
-
-
-""" Hyperparamters """
-
-bs = 40
+""" Hyperparameter """
 lr = 1e-3
-num_cl = 2
+n_class = 2
 drop = 0.5
 total_epoch = 100
-w_decay = 0.1
-scenario = 1
-gpu = 0
-fold = 1
-data = "GIST"
-network = "EEGNet"
+w_decay = 0.05
 alpha = 0.5
 beta = 0.3
 gamma = 0.5
+bs = 40
